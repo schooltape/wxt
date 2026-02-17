d15c39c9146a9df30f82c2175b6a91b6ee56dc9f
willow <42willow@pm.me>
2026-02-17 19:59:10 +1100

    chore(storage): @schooltape/wxt-storage

--- packages/wxt/src/utils/storage.ts
+++ packages/wxt/src/utils/storage.ts
@@ -2,4 +2,4 @@
  * Re-export the [`@wxt-dev/storage` package](https://www.npmjs.com/package/@wxt-dev/storage).
  * @module wxt/utils/storage
  */
-export * from '@wxt-dev/storage';
+export * from '@schooltape/wxt-storage';
